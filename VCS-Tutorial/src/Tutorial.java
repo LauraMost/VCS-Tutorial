049d15ab89e5d7f59cfdf4bd28ad08a48ed8ac8a
LauraMost <1423841@stud.hs-mannheim.de>
2017-10-15 12:07:35 +0200

    Added user name for personal greeting

--- VCS-Tutorial/src/Tutorial.java
+++ VCS-Tutorial/src/Tutorial.java
@@ -2,8 +2,10 @@
 public class Tutorial {
 
 	public static void main(String[] args) {
+
+		String user = "Laura";
 		
-		System.out.println("Welcome to the Introduction to Version Control Systems!");
+		System.out.println("Welcome, "+user+", to the Introduction to Version Control Systems!");
 		
 	}
 
